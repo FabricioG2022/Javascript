3a12a7bcec361a0d7bf42a2ba5dc5f48a4b2fb11
Fabricio Godoy <firgodoy@hotmail.com>
2022-08-21 14:56:32 -0300

    Interactuando con el Dom

---
+++ app.js
@@ -0,0 +1,8 @@
+let div = document.getElementById('prueba')
+
+div.innerHTML = prompt ("Ingresa tu nombre y edad").toUpperCase ()
+
+document.body.append()
+
+let saludo = prompt ("Muchas gracias")
+
