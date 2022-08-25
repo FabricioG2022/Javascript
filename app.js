56c8bf825c190c42830f8963ea8ed4bbfbeb18b7
Fabricio Godoy <firgodoy@hotmail.com>
2022-08-25 20:02:41 -0300

    Dom + eventos

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-let div = document.getElementById('prueba')
 
-div.innerHTML = prompt ("Ingresa tu nombre y edad").toUpperCase ()
 
-document.body.append()
+let formulario = document.getElementById('formulario')
 
-let saludo = prompt ("Muchas gracias")
+
+formulario.addEventListener('submit', datosSeleccionados)
+
+function datosSeleccionados(e) {
+    e.preventDefault();
+    const datos = e.target
+    const destino = document.getElementById("destino").value
+    const lugar = document.getElementById("lugar").value
+    const personas = document.getElementById("personas").value
+    const fechsal = document.getElementById("fechsal").value
+    const fechlleg = document.getElementById("fechlleg").value
+    const eleccion = document.getElementById("eleccion");
+    eleccion.textContent = `Hola tus datos ingresados son: ${destino}, ${lugar}, Cantidad de personas: ${personas}, Salis: ${fechsal}, Llegas: ${fechlleg}`.toUpperCase();
+    if (destino == "" || lugar == "" || personas == "" || fechsal == "" || fechlleg == "") {
+        alert ("Debe ingresar los datos correspondientes")
+        eleccion.textContent = ""
+        document.getElementById("destino").focus()
+    } else {
+        document.getElementById("destino").value = "";
+        document.getElementById("lugar").value = "";
+        document.getElementById("personas").value = "";
+        document.getElementById("fechsal").value = "";
+        document.getElementById("fechlleg").value = "";
+        document.getElementById("destino").focus()
+    }
+}
 
